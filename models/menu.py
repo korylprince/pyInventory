b21c84453ea37c445cf1a02ed74062131f3fb211
Kory Prince <korylprince@gmail.com>
2012-04-12 16:32:28 -0500

    only show menu if logged in

--- models/menu.py
+++ models/menu.py
@@ -3,9 +3,11 @@
 #########################################################################
 ## this is the main application menu add/remove items as required
 #########################################################################
-
-response.menu = [
-    (T('Home'), False, URL('default','index'), []),
-    (T('Add'), False, URL('default','add'), []),
-    (T('Search'), False, URL('default','search'), [])
-    ]
+if auth.is_logged_in():
+    response.menu = [
+        (T('Home'), False, URL('default','index'), []),
+        (T('Add'), False, URL('default','add'), []),
+        (T('Search'), False, URL('default','search'), [])
+        ]
+else:
+    response.menu = [(T('Home'), False, URL('default','index'), [])]
